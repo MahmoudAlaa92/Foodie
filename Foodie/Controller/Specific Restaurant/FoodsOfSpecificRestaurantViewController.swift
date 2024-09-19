e8c6343453d84d04d72527f665e5480a7cd2d88a
MahmoudAlaa <mahmoudalaa.wr@gmail.com>
2024-09-19 03:45:37 +0300

    Add Firebase

---
+++ Foodie/Controller/Specific Restaurant/FoodsOfSpecificRestaurantViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FoodsOfSpecificRestaurantViewController.swift
+//  Foodie
+//
+//  Created by Mahmoud Alaa on 17/09/2024.
+//
+
+import UIKit
+
+class FoodsOfSpecificRestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
